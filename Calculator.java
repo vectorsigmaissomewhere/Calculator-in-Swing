eb26a2dc39e4754c5dab8ff2165d04be799bddea
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-13 04:17:52 -0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,124 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CalculatorPanel extends JPanel {
+    private JButton display;
+    private JPanel panel;
+    private double result;
+    private String lastCommand;
+    private boolean start;
+
+    public CalculatorPanel() {
+        setLayout(new BorderLayout());
+        result = 0;
+        lastCommand = "=";
+        start = true;
+
+        display = new JButton("0");
+        display.setEnabled(false);
+        add(display, BorderLayout.NORTH);
+
+        ActionListener insert = new InsertAction();
+        ActionListener command = new CommandAction();
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4));
+
+        addButton("7", insert);
+        addButton("8", insert);
+        addButton("9", insert);
+        addButton("/", command);
+
+        addButton("4", insert);
+        addButton("5", insert);
+        addButton("6", insert);
+        addButton("*", command);
+
+        addButton("1", insert);
+        addButton("2", insert);
+        addButton("3", insert);
+        addButton("-", command);
+
+        addButton("0", insert);
+        addButton(".", insert);
+        addButton("=", command);
+        addButton("+", command);
+
+        add(panel, BorderLayout.CENTER);
+    }
+
+    private void addButton(String label, ActionListener listener) {
+        JButton button = new JButton(label);
+        button.addActionListener(listener);
+        panel.add(button);
+    }
+
+    private class InsertAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String input = e.getActionCommand();
+            if (start) {
+                display.setText("");
+                start = false;
+            }
+            display.setText(display.getText() + input);
+        }
+    }
+
+    private class CommandAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String command = event.getActionCommand();
+
+            if (start) {
+                if (command.equals("-")) {
+                    display.setText(command);
+                    start = false;
+                } else {
+                    lastCommand = command;
+                }
+            } else {
+                calculate(Double.parseDouble(display.getText()));
+                lastCommand = command;
+                start = true;
+            }
+        }
+    }
+
+    public void calculate(double x) {
+        switch (lastCommand) {
+            case "+":
+                result += x;
+                break;
+            case "-":
+                result -= x;
+                break;
+            case "*":
+                result *= x;
+                break;
+            case "/":
+                if (x != 0) {
+                    result /= x;
+                } else {
+                    display.setText("Error");  // Handle division by zero
+                    return;
+                }
+                break;
+            case "=":
+                result = x;
+                break;
+        }
+        display.setText("" + result);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+
+        CalculatorPanel calculator = new CalculatorPanel();
+        frame.add(calculator);
+
+        frame.setVisible(true);
+    }
+}
+
